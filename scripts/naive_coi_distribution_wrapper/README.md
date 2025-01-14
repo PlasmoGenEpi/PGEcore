a45402be67a8aba00f81c17a4556244d338b35ce
Maxwell Murphy <mm@maxmurphy.dev>
2025-01-14 16:10:44 -0500

    adding naive coi distribution summary

---
+++ scripts/naive_coi_distribution_wrapper/README.md
@@ -0,0 +1,16 @@
+# Naive COI Distribution Estimation
+
+Contents: 
+* [Tool Information](#tool-information)
+* [Script Usage](#script-usage)
+
+## Tool Information
+
+This tool provides a naive summary of the distribution of COI calls across samples.
+
+## Script Usage 
+```
+Rscript naive_coi_distribution_wrapper.R \
+  --coi_calls data/example_coi_table.tsv \
+  --output coi_distribution.tsv
+```
---
+++ scripts/naive_coi_distribution_wrapper/naive_coi_distribution_wrapper.R
@@ -0,0 +1,52 @@
+library(optparse)
+library(rlang)
+
+options(dplyr.summarise.inform = FALSE)
+
+opts <- list(
+  make_option(
+    c("-i", "--coi_calls"),
+    help = "TSV containing coi calls, with the columns: specimen_id, coi ",
+    type = "character",
+    default = NULL,
+    callback = function(opt, flag_string, value, parser, ...) {
+      if (!file.exists(value)) {
+        stop(stringr::str_c(opt$coi_calls, " does not exist"))
+      }
+      value
+    }
+  )
+)
+
+args <- parse_args(OptionParser(option_list = opts))
+required_args <- c("coi_calls")
+missing_args <- setdiff(required_args, names(args))
+if (length(missing_args) > 0) {
+  stop(stringr::str_c("Missing required arguments: ", missing_args))
+}
+
+
+load_coi_calls <- function(path) {
+  coi_dat <- readr::read_tsv(
+    path,
+    col_types = readr::cols(
+      specimen_id = readr::col_character(),
+      coi = readr::col_integer()
+    ),
+    col_select = c("specimen_id", "coi")
+  )
+  return(coi_dat)
+}
+
+calculate_coi_distribution <- function(coi_calls) {
+  coi_dist <- coi_calls |>
+    dplyr::group_by(.data$coi) |>
+    dplyr::summarise(n = dplyr::n()) |>
+    dplyr::mutate(prop = .data$n / sum(.data$n))
+  return(coi_dist)
+}
+
+coi_calls <- load_coi_calls(args$coi_calls)
+coi_dist <- calculate_coi_distribution(coi_calls)
+
+readr::write_tsv(coi_dist, args$output)
