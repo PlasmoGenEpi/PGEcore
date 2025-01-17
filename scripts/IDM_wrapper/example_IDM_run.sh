bd95ce603957186a158d32b0f9204f9ffa9ae8e2
Alfred Hubbard <ahubba19@jh.edu>
2025-01-17 16:21:37 -0500

    Remove shell script for running wrapper script

--- scripts/IDM_wrapper/example_IDM_run.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-# start from the root folder
-cd $(git rev-parse --show-toplevel)
-cd scripts/IDM_wrapper/
-
-Rscript ./IDM_wrapper.R --aa_calls_input ../../data/example_amino_acid_calls.tsv --model IDM --slaf_output out_slaf.tsv --eps_initial 0.1
