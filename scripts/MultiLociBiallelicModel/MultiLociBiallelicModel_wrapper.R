d2ed0910774cdd4126b5b046b74d4ff70233b1c3
Jorge Romero <jeamaya1991@gmail.com>
2025-01-19 12:11:01 -0500

    Added functionality to separate calculation by groups. Updated readme.md and doc

--- scripts/MultiLociBiallelicModel/MultiLociBiallelicModel_wrapper.R
+++ scripts/MultiLociBiallelicModel/MultiLociBiallelicModel_wrapper.R
@@ -12,6 +12,14 @@ opts = list(
       "TSV containing aminoacid calls per specimen, with the columns: specimen_id,
       target_id, specimen_id, gene_id, aa_position, ref_aa, and aa."
     )
+  ),
+  make_option(
+    "--loci_group_table",
+    type = "character", 
+    help = str_c(
+      "TSV containing loci groups, with the columns: group_id, gene_id, 
+      aa_position."
+    )
   )
 )
 
@@ -19,39 +27,46 @@ arg <- parse_args(OptionParser(option_list = opts))
 
 # MultiLociBiallelicModel_wrapper functions ----------------------------
 
-#' Create Input for Multi-Loci Biallelic Model
+#' Create Multi-Loci Biallelic Model Input
 #'
-#' This function reads an allele table from a file, processes it to create an input dataset for a multi-loci biallelic model, and validates the resulting data structure.
+#' This function processes allele data and generates the input required for multi-loci biallelic analysis.
+#' It validates input data, processes amino acid information, matches loci groups, and creates grouped
+#' subtables for downstream analysis.
 #'
-#' @param input_path A string specifying the file path to the input data. The file should be a tab-separated CSV file with columns including `specimen_id`, `gene_id`, `aa_position`, `ref_aa`, and `aa`.
+#' @param input_path A string specifying the path to the input allele table (in tab-delimited format).
+#' @param loci_group A string specifying the path to the loci group file (in tab-delimited format).
 #'
-#' @return A data frame that has been processed to include:
+#' @return A list (`MLBM_object`) containing:
 #' \describe{
-#'   \item{specimen_id}{The specimen identifiers from the input file.}
-#'   \item{identifier}{A unique identifier for each position, combining `gene_id` and `aa_position`.}
-#'   \item{value}{An integer value for each entry:
-#'     \itemize{
-#'       \item \code{0}: All entries for the position have `ref_aa == aa`.
-#'       \item \code{1}: All entries for the position have `ref_aa != aa`.
-#'       \item \code{2}: Mixed entries, where some have `ref_aa == aa` and others have `ref_aa != aa`.
-#'       \item \code{NA}: Positions with more than two distinct amino acids (`aa`).}}
+#'   \item{`MLBM_data`}{A processed tibble containing the input allele data in a wide format.}
+#'   \item{`staves_data`}{A tibble mapping each allele to its state (reference or alternative).}
+#'   \item{`by_group_table`}{A list of tibbles, where each tibble corresponds to a group of loci and contains the associated allele data.}
+#'   \item{`groups`}{A character vector of unique `group_id` values.}
 #' }
 #'
 #' @details
-#' The function applies validation rules to ensure that all values in the resulting data frame (except `specimen_id`) are integers in the range \code{0} to \code{2}, or \code{NA}. If any validation checks fail, the function raises an error and stops execution.
+#' This function performs the following steps:
+#' \enumerate{
+#'   \item Reads the allele table from `input_path` and processes it to create a wide-format tibble.
+#'   \item Validates the allele data to ensure all loci columns (excluding `specimen_id`) are numeric.
+#'   \item Processes the loci group file from `loci_group` and validates it against predefined rules.
+#'   \item Matches loci from the allele table to their corresponding groups, creating subtables for each group.
+#'   \item Generates a `MLBM_object` containing all processed data and grouped subtables.
+#' }
 #'
 #' @examples
-#' \dontrun{
-#' # Example usage:
-#' input_path <- "path/to/allele_table.csv"
-#' model_input <- create_MultiLociBiallelicModel_input(input_path)
-#' }
+#' # Define file paths
+#' input_path <- "allele_table.tsv"
+#' loci_group <- "loci_group.tsv"
+#'
+#' # Generate the Multi-Loci Biallelic Model input
+#' MLBM_object <- create_MultiLociBiallelicModel_input(input_path, loci_group)
 #'
 #' @import dplyr
 #' @import tidyr
 #' @import validate
 #' @export
-create_MultiLociBiallelicModel_input <- function(input_path) {
+create_MultiLociBiallelicModel_input <- function(input_path, loci_group) {
   # Read the allele table
   print("Reading input data")
   input_data <- read.csv(input_path, na.strings = "NA", sep = "\t")
@@ -96,9 +111,64 @@ create_MultiLociBiallelicModel_input <- function(input_path) {
     select(staves, state) %>%
     mutate(prestave = sub(":([^:]+)$", "", staves))
   
-  
   MLBM_object = list(MLBM_data = MLBM_data, 
                      staves_data = staves_data)
+  
+  loci_groups = read.csv(loci_group, sep = "\t")
+  
+  rules <- validate::validator(
+    # Data columns
+    is.character(group_id),
+    is.character(gene_id),
+    is.integer(aa_position),
+    
+    # Non-missing values
+    !is.na(group_id),
+    !is.na(gene_id),
+    !is.na(aa_position)
+  )
+  
+  # Confront the analysis_object with validation rules
+  print("Confronting input data with validation rules")
+  fails <- validate::confront(loci_groups, rules, raise = "all") %>%
+    validate::summary() %>%
+    dplyr::filter(fails > 0)
+  
+  # Raise an error if any validations fail
+  if (nrow(fails) > 0) {
+    stop(
+      "Analysis object failed one or more validation checks: ",
+      str_c(fails$expression, collapse = "\n"),
+      call. = FALSE
+    )
+  }
+  
+  #Matching groups to create lists of loci
+  match_group = input_data %>% 
+    select(target_id, gene_id, aa_position) %>%
+    mutate(identifier = paste(target_id, aa_position, sep = ":")) 
+  
+  merged_data <- match_group %>%
+    inner_join(loci_groups, by = c("gene_id", "aa_position"), relationship = "many-to-many")
+  
+  grouped_list <- merged_data %>%
+    group_by(group_id) %>%
+    summarise(identifiers = list(unique(identifier)), .groups = "drop")
+  
+  result_list <- setNames(grouped_list$identifiers, grouped_list$group_id)
+  
+  # Generate subtables
+  result_tables <- lapply(names(result_list), function(group_name) {
+    columns <- c("specimen_id", result_list[[group_name]])
+    subtable <- MLBM_data %>% select(all_of(columns))
+    return(subtable)
+  })
+  
+  # Name the list elements
+  names(result_tables) <- names(result_list)
+  MLBM_object$by_group_table <- result_tables
+  MLBM_object$groups = unique(loci_groups$group_id)
+  
   return(MLBM_object)
 }
 
@@ -156,50 +226,44 @@ run_MultiLociBiallelicModel <- function(inputToMLE) {
 
 #' Summarise Multi-Loci Biallelic Model Results
 #'
-#' This function processes and summarises results from the Multi-Loci Biallelic Model (MLBM). It updates the sequence matrix with corresponding values from the `staves_data`, modifies the sequence representation, and writes the summarised results to a TSV file.
+#' This function processes and summarises results from a Multi-Loci Biallelic Model (MLBM) analysis.
+#' It modifies the sequence representation in `MLBM_res` based on `MLBM_object` and the provided `group_name`,
+#' updating the `plsf_table` with new sequence variants and frequencies.
 #'
-#' @param MLBM_res A list containing the results of the MLBM analysis, including:
+#' @param MLBM_res A list containing the results of the MLBM analysis, including the `plsf_table`.
+#' @param MLBM_object A list containing the input data and metadata used in the MLBM analysis, including `by_group_table` and `staves_data`.
+#' @param group_name A string specifying the name of the group to process, corresponding to a key in `MLBM_object$by_group_table`.
+#'
+#' @return The modified `MLBM_res` object with an updated `plsf_table` that includes:
 #' \describe{
-#'   \item{\code{plsf_table}}{A data frame with columns \code{sequence} (encoded as a string of 0s and 1s) and \code{MLBM_frequency} (the frequency of the sequence).}
-#' }
-#' @param MLBM_object A list containing the MLBM data and additional information, including:
-#' \describe{
-#'   \item{\code{MLBM_data}}{A data frame where each column corresponds to a specific genetic locus and each row to a specimen.}
-#'   \item{\code{staves_data}}{A data frame with columns:
-#'     \itemize{
-#'       \item \code{staves}: The encoded genetic representation.
-#'       \item \code{state}: The state (0 or 1) of the representation.
-#'       \item \code{prestave}: The genetic locus identifier without the encoded suffix.}}
+#'   \item{`group_id`}{A column indicating the group name.}
+#'   \item{`variant`}{A column representing the reconstructed sequence variants.}
+#'   \item{`freq`}{A column indicating the frequency of each variant.}
 #' }
 #'
 #' @details
 #' The function performs the following steps:
 #' \enumerate{
-#'   \item Converts the \code{sequence} column in \code{MLBM_res$plsf_table} into a matrix, splitting each sequence into individual loci.
-#'   \item Replaces 0s and 1s in the sequence matrix with corresponding genetic representations from \code{MLBM_object$staves_data}.
-#'   \item Updates the \code{sequence} column in \code{MLBM_res$plsf_table} by concatenating all loci values row-wise, separated by semicolons (\code{;}).
-#'   \item Renames columns in \code{MLBM_res$plsf_table} to \code{seq} and \code{freq} for clarity.
-#'   \item Writes the summarised table to a TSV file named \code{"MLBM_summary.tsv"} in the working directory.
+#'   \item Splits sequences in `MLBM_res$plsf_table` into individual characters and converts them to a wide format.
+#'   \item Matches and replaces sequence states using `staves_data` in `MLBM_object`.
+#'   \item Reconstructs updated sequences by combining individual character columns into a single string.
+#'   \item Updates the `plsf_table` with new `group_id`, `variant`, and `freq` columns.
 #' }
-#'
-#' @return None. The function modifies the input \code{MLBM_res} and writes the summarised results to a file.
 #'
 #' @examples
-#' \dontrun{
-#' # Example usage:
-#' summarise_MLBM_results(MLBM_res, MLBM_object)
-#' }
+#' # Example usage
+#' summarised_results <- summarise_MLBM_results(MLBM_res, MLBM_object, "pfdhfr_pfdhps")
 #'
 #' @import dplyr
 #' @import tidyr
-#' @import readr
+#' @import stringr
 #' @export
-summarise_MLBM_results <- function(MLBM_res, MLBM_object) {
+summarise_MLBM_results <- function(MLBM_res, MLBM_object, group_name) {
   sequence_matrix <- MLBM_res$plsf_table %>%
     mutate(sequence_split = strsplit(sequence, "")) %>%  # Split each sequence into characters
     tidyr::unnest_wider(sequence_split, names_sep = "_") %>%   # Convert the list of characters to columns
     select(-MLBM_frequency) %>%                         # Remove the frequency column
-    rename_with(~ colnames(MLBM_object$MLBM_data)[-1], starts_with("sequence_split")) %>% # Rename columns
+    rename_with(~ colnames(MLBM_object$by_group_table[[group_name]])[-1], starts_with("sequence_split")) %>% # Rename columns
     as.matrix() 
   
   # Convert sequence_matrix to a data frame for easier manipulation
@@ -220,23 +284,25 @@ summarise_MLBM_results <- function(MLBM_res, MLBM_object) {
   
   MLBM_res$plsf_table$sequence <- apply(sequence_df[-1], 1, paste, collapse = ";")
   
+  # Add group_id column and rename columns
   MLBM_res$plsf_table <- MLBM_res$plsf_table %>%
-    rename(seq = sequence, 
-           freq = MLBM_frequency)
-  
-  readr::write_tsv(MLBM_res$plsf_table, "MLBM_summary.tsv")
+    mutate(group_id = group_name, .before = 1) %>% # Add group_id at the beginning
+    rename(variant = sequence, freq = MLBM_frequency) # Rename columns
 }
 
 # Main -----------------------------------------------------------------
 
 # Read the aminoacid calls
-#arg$aa_calls = "example_amino_acid_calls.tsv"
 
-MLBM_object <- create_MultiLociBiallelicModel_input(arg$aa_calls)
+MLBM_object <- create_MultiLociBiallelicModel_input(arg$aa_calls, arg$loci_group)
 
-# Run the Multi-Loci Biallelic Model
-MLBM_res <- run_MultiLociBiallelicModel(MLBM_object$MLBM_data)
+MLBM_res = list()
+# Iterate over each table by groups of loci
+for (group_name in MLBM_object$groups) {
+  print(paste0("Running MultiLociBiallelicModel on ", group_name))
+  group_table <- MLBM_object$by_group_table[[group_name]]
+  MLBM_tmp <- run_MultiLociBiallelicModel(group_table)
+  MLBM_res[[group_name]] <- summarise_MLBM_results(MLBM_tmp, MLBM_object, group_name)
+}
 
-# Generate summary
-summarise_MLBM_results(MLBM_res, MLBM_object)
-
+readr::write_tsv(bind_rows(MLBM_res), "MLBM_summary.tsv")
