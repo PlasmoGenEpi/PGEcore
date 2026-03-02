d8071810ad807fb531a3431b24be7bc17fc6c62e
Alfred Hubbard <ahubba19@jh.edu>
2026-03-02 11:45:34 -0500

    Initial functional version of wrapper

---
+++ scripts/snpslice_wrapper/README.md
@@ -0,0 +1,53 @@
+# SNP-Slice Wrapper
+
+Contents: 
+* [Tool Information](#tool-information)
+* [Script Usage](#script-usage)
+
+## Tool Information
+
+SNP-Slice is a Bayesian nonparametric method for resolving multi-strain 
+infections using slice sampling. The algorithm simultaneously estimates strain 
+haplotypes and links them to hosts from SNP data.
+
+This wrapper runs SNP-Slice and calculates population-level multilocus allele 
+frequencies (MLAF) and sample-level complexity of infection (COI) estimates from 
+the SNP-Slice results. This functionality is supported by the 
+[snp.slicer](https://github.com/PlasmoGenEpi/snp.slicer) R package, which is a 
+repackage of the original SNP-Slice code.
+
+### Existing Resources
+
+SNP-Slice is described in: Ju, N., Liu, J., & He, Q. (2024). SNP-slice resolves 
+mixed infections: Simultaneously unveiling strain haplotypes and linking them to 
+hosts. Bioinformatics (Oxford, England), 40(6), btae344. 
+https://doi.org/10.1093/bioinformatics/btae344
+
+The original SNP-Slice code can be found 
+[here](https://github.com/nianqiaoju/snp-slice) and the snp.slicer repackaging 
+of this code is [here](https://github.com/PlasmoGenEpi/snp.slicer).
+
+## Inputs and Outputs
+
+The script requires an allele table (`--allele_table`) and a loci groups table 
+(`--loci_groups_input`). The allele table should only contain biallelic 
+genotypes. A variety of optional inputs specify the column names in these tables 
+and parameters for running SNP-Slice. For specifics on input formats, refer to 
+the argument documentation in the script.
+
+The MLAF output will be a TSV with group\_id, variant, freq, sample\_total, and 
+allele\_total columns. The COI output will be a TSV with a column matching 
+`--specimen_id_col` and coi, coi\_sd, coi\_lower, and coi\_upper columns. The 
+latter three are the standard deviation and lower and upper bounds of the 95% 
+credible interval of the COI estimate. Note these columns will be NA if the 
+`--use_mcmc_for_af_and_coi` flag is not used.
+
+## Script Usage
+
+```{r}
+scripts/snpslice_wrapper/snpslice_wrapper.R \
+    --allele_table data/example_amino_acid_calls.tsv \
+    --loci_groups data/example_loci_groups.tsv \
+    --mlaf_output mlaf.tsv \
+    --coi_output coi.tsv
+```
--- scripts/snpslice_wrapper/snpslice_wrapper.R
+++ scripts/snpslice_wrapper/snpslice_wrapper.R
@@ -9,10 +9,25 @@ library(snp.slicer)
 library(dplyr, warn.conflicts = FALSE)
 library(magrittr)
 library(optparse)
+library(purrr, warn.conflicts = FALSE)
 library(readr)
 library(stringr)
 library(tibble)
 library(tidyr, warn.conflicts = FALSE)
+
+# install a specific version of variantstring package (this is in development so
+# may not always be backwards-compatible)
+variantstring_version <- "1.8.0"
+if (!requireNamespace("variantstring", quietly = TRUE) ||
+  packageVersion("variantstring") != variantstring_version) {
+  stop(
+    "This script requires variantstring version ",
+    variantstring_version,
+    " and it is not installed",
+    call. = FALSE
+  )
+}
+library(variantstring)
 
 # Parse arguments ------------------------------------------------------
 opts <- list(
@@ -33,7 +48,7 @@ opts <- list(
   ), 
   make_option(
     "--target_id_col", 
-    default = "target_id", 
+    default = "aa_locus", 
     help = 
       str_c(
         "String giving the name of the target ID column (e.g., the locus ", 
@@ -42,7 +57,7 @@ opts <- list(
   ), 
   make_option(
     "--target_value_col", 
-    default = "seq", 
+    default = "aa", 
     help = 
       str_c(
         "String giving the name of the target value column (e.g., the allele ", 
@@ -51,7 +66,7 @@ opts <- list(
   ), 
   make_option(
     "--target_count_col", 
-    default = "seq", 
+    default = "read_count", 
     help = 
       str_c(
         "String giving the name of the target count column (e.g., the read ", 
@@ -114,10 +129,19 @@ opts <- list(
     )
   ), 
   make_option(
-    "--store_mcmc", 
+    "--use_mcmc_for_af_and_coi", 
     action = "store_true", 
     default = FALSE, 
-    help = "Whether to store full MCMC samples (default: FALSE)"
+    help = str_c(
+      "For calculating COI and allele frequencies, whether to sample from ", 
+      "MCMC (TRUE) or use MAP estimates (FALSE; default)."
+    )
+  ), 
+  make_option(
+    c("-v", "--verbose"), 
+    action = "store_true", 
+    default = FALSE, 
+    help = "Verbose output."
   ), 
   make_option(
     "--seed", 
@@ -144,12 +168,13 @@ opts <- list(
 arg <- parse_args(OptionParser(option_list = opts))
 # Arguments used for development
 if (interactive()) {
-  arg$allele_table <- "../../data/example_amino_acid_calls.tsv"
+  arg$allele_table <- "../../data/example2_amino_acid_calls.tsv"
   arg$loci_groups_input <- "../../data/example_loci_groups.tsv"
   arg$target_id_col <- "aa_locus"
   arg$target_value_col <- "aa"
   arg$target_count_col <- "read_count"
   arg$n_mcmc <- 100
+  arg$use_mcmc_for_af_and_coi <- TRUE
 }
 
 #' Read allele table into a tibble
@@ -197,7 +222,6 @@ create_allele_table_input <- function(
       target_count = all_of(target_count_col)
     )
 
-    print(allele_table)
   # Validate fields
   rules <- validate::validator(
     is.character(specimen_id), 
@@ -232,6 +256,8 @@ create_allele_table_input <- function(
 #'
 #' @param loci_groups_path Path to loci groups TSV. It should have 
 #'   a column for group_id and a column matching target_id_col.
+#' @param allele_table A tibble produced by 
+#'   `create_allele_table_input()`.
 #' @inheritParams create_allele_table_input
 #'
 #' @import dplyr
@@ -240,6 +266,7 @@ create_allele_table_input <- function(
 #'   target_id.
 create_loci_group_input <- function(
                                     loci_groups_path, 
+                                    allele_table, 
                                     target_id_col = "target_id") {
 
   # Check input arguments
@@ -271,11 +298,112 @@ create_loci_group_input <- function(
     )
   }
 
+  # Convert to list format
+  loci_groups <- split(loci_groups$target_id, loci_groups$group_id)
+
+  # Check for non-biallelic loci
+  loci_groups_copy <- loci_groups
+  for (lg in names(loci_groups_copy)) {
+    non_biallelic_trgs <- allele_table %>%
+      filter(target_id %in% loci_groups_copy[[lg]]) %>%
+      group_by(target_id) %>%
+      filter(n_distinct(target_value) > 2) %$%
+      unique(target_id)
+    if (length(non_biallelic_trgs > 0)) {
+      warning(
+        "The targets ", 
+        str_c(non_biallelic_trgs, collapse = ", "), 
+        " in the group ", 
+        lg, 
+        " have more than two alleles and this group will be excluded.", 
+        call. = FALSE
+      )
+      loci_groups[[lg]] <- NULL
+    }
+  }
+
   return(loci_groups)
 
 }
 
-options(error = stop)
+#' Calculate and format allele frequencies from SNP-Slice results
+#'
+#' This function takes a snp.slicer results object and a list of loci 
+#' groups, calculates the allele frequencies for each group, and 
+#' formats the output into a tibble suitable for writing to disk.
+#'
+#' @param snp_slice_res A snp.slicer results object produced by 
+#'   `snp.slicer::snp_slice()`.
+#' @param loci_groups A list containing named character vectors 
+#'   defining loci groups.
+#' @param use_mcmc Logical indicating whether to use the MCMC results 
+#'   for calculating allele frequencies.
+#'
+#' @return A tibble with group_id, variant, freq, allele_total, and 
+#'   sample_total columns.
+prepare_af_output <- function(
+                              snp_slice_res, 
+                              loci_groups, 
+                              use_mcmc) {
+
+  # Reformat a tibble of allele frequencies to have variant string names
+  format_af_table_w_variantstring <- function(af_table, group_id, loci_groups) {
+    # Format a multi-locus genotype into long form for variantstring
+    prep_variantstring_input <- function(allele, loci_names) {
+      aa <- str_split_1(allele, "\\|")
+      tibble(gene_pos = loci_names, aa = aa) %>%
+        separate_wider_delim(gene_pos, ":", names = c("gene", "pos")) %>%
+        mutate(pos = as.integer(pos)) %>%
+        mutate(n_aa = 1, het = FALSE, phased = TRUE, read_count = NA) %>%
+        relocate(aa, .before = read_count)
+    }
+
+    # Convert allele names to variant string format
+    af_table %>%
+      as_tibble() %>%
+      # All possible genotypes will be included, but many will have a 
+      # freq of 0 if use_mcmc = FALSE or some samples are missing loci
+      filter(frequency > 0) %>%
+      mutate(
+        allele = map(
+          allele, 
+          prep_variantstring_input, 
+          loci_groups[[group_id]]
+        )
+      ) %>%
+      mutate(allele = variantstring::long_to_variant(allele))
+  }
+
+  # Calculate allele frequencies
+  snp_slicer_af_by_group <- snp.slicer::calculate_allele_frequencies_by_sets(
+      snpslice_res, 
+      loci_groups, 
+      use_map = ! use_mcmc
+    )
+  # Reformat
+  tibble(
+      group_id = names(snp_slicer_af_by_group), 
+      af_tib = snp_slicer_af_by_group
+    ) %>%
+    mutate(
+      af_tib = map2(
+        af_tib, 
+        group_id, 
+        format_af_table_w_variantstring, 
+        loci_groups
+      )
+    ) %>%
+    unnest(af_tib) %>%
+    rename(
+      variant = allele, 
+      freq = frequency, 
+      sample_total = count, 
+      allele_total = total_parasites
+    )
+
+}
+
+
 # Read inputs ----------------------------------------------------------
 allele_table <- create_allele_table_input(
   arg$allele_table, 
@@ -286,9 +414,11 @@ allele_table <- create_allele_table_input(
 )
 loci_groups <- create_loci_group_input(
   arg$loci_groups_input, 
+  allele_table, 
   target_id_col = arg$target_id_col
 )
 
+# Run SNP-Slice --------------------------------------------------------
 snpslice_res <- snp.slicer::snp_slice(
   allele_table, 
   model = arg$model, 
@@ -298,9 +428,24 @@ snpslice_res <- snp.slicer::snp_slice(
   rho = arg$rho, 
   threshold = arg$threshold, 
   gap = arg$gap, 
-  store_mcmc = arg$store_mcmc, 
+  store_mcmc = TRUE, 
+  verbose = arg$v, 
   specimen_id_col = "specimen_id", 
   target_id_col = "target_id", 
   target_value_col = "target_value", 
   target_count_col = "target_count"
 )
+
+# Calculate and write allele frequencies -------------------------------
+snpslice_res %>%
+  prepare_af_output(loci_groups, arg$use_mcmc_for_af_and_coi) %>%
+  write_tsv(arg$mlaf_output)
+
+# Calculate and write COI ----------------------------------------------
+snpslice_res %>%
+  snp.slicer::calculate_individual_coi(
+    use_map = ! arg$use_mcmc_for_af_and_coi
+  ) %>%
+  select(-host_index) %>%
+  rename(!! arg$specimen_id_col := host_id, coi = coi_estimate) %>%
+  write_tsv(arg$coi_output)
