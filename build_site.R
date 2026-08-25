42419c4c4ba5d7db6d4f3ca1d82dcac7ed8de647
kathryn1995 <kathryn.murie@ucsf.edu>
2026-08-25 07:33:06 -0700

    remove locally used build script

--- build_site.R
+++
@@ -1,8 +0,0 @@
-# once
-install.packages("pkgdown", repos = "https://cloud.r-project.org")
-devtools::install()   # or: pak::local_install()
-
-# build + open in browser
-pkgdown::build_site()
-
-browseURL("docs/index.html")
